1eec8823e21e149ffee4cced355bd4d0cb68ccc0
Gaston Asis <gastonasis@MacBook-Pro-de-Gaston.local>
2020-08-24 20:41:40 -0300

    Updating use case

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import Home from "./views/Home";
 import Profile from "./views/Profile";
-import ExternalApi from "./views/ExternalApi";
 import { useAuth0 } from "@auth0/auth0-react";
 import history from "./utils/history";
 
@@ -22,7 +21,11 @@ const App = () => {
   const { isLoading, error } = useAuth0();
 
   if (error) {
-    return <div>Oops... {error.message}</div>;
+    if (error.error == 'unauthorized') {
+      console.log("ER", error.error);
+    } else {
+      return <div>Oops... {error.message}</div>;
+    }
   }
 
   if (isLoading) {
@@ -37,7 +40,6 @@ const App = () => {
           <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/profile" component={Profile} />
-            <Route path="/external-api" component={ExternalApi} />
           </Switch>
         </Container>
         <Footer />
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,17 +1,28 @@
 import React from "react";
 
+import { useAuth0 } from "@auth0/auth0-react";
+
 import logo from "../assets/logo.svg";
 
-const Hero = () => (
-  <div className="text-center hero my-5">
-    <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" />
-    <h1 className="mb-4">React.js Sample Project</h1>
+const Hero = () => {
+  const { isLoading, error } = useAuth0();
 
-    <p className="lead">
-      This is a sample application that demonstrates an authentication flow for
-      an SPA, using <a href="https://reactjs.org">React.js</a>
-    </p>
-  </div>
-);
+  if (error) {
+    if (error.error == 'unauthorized') {
+      return <div>Oops... {error.message}</div>;
+    }
+  }
+  
+  return (
+    <div className="text-center hero my-5">
+      <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" />
+      <h1 className="mb-4">Cruise0</h1>
+
+      <p className="lead">    
+        Let's travel the world!
+      </p>
+    </div>
+  );
+};
 
 export default Hero;
