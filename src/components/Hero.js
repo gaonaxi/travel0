337fbc4d8f66d51c2c018a01eec139943f8148b6
Gaston Asis <gastonasis@MacBook-Pro-de-Gaston.local>
2020-09-01 23:01:16 -0300

    Updating

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -15,7 +15,7 @@ const Hero = () => {
   
   return (
     <div className="text-center hero my-5">
-      <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" />
+      <img className="mb-3 app-logo" src={logo} alt="Cruise0" width="120" />
       <h1 className="mb-4">Cruise0</h1>
 
       <p className="lead">    
--- tenant/rules/age-range.js
+++ tenant/rules/age-range.js
@@ -1,18 +1,20 @@
 function (user, context, callback) {
-    let ageRange = user.age_range || undefined;
-    
-    if (!ageRange) {
-      const _ = require('lodash');
-      const facebookIdentity = _.find(user.identities, { connection: 'facebook' });
+  let ageRange = user.age_range || undefined;
   
-      if (facebookIdentity) {
-       ageRange = facebookIdentity.age_range; 
-      }
+  if (!ageRange) {
+    const _ = require('lodash');
+    const facebookIdentity = _.find(user.identities, { connection: 'facebook' });
+
+    console.log(facebookIdentity);
+    
+    if (facebookIdentity) {
+     ageRange = facebookIdentity.age_range; 
     }
-    
-    context.idToken['https://cruise0.com/analytics'] = {
-      age_range: ageRange || 'Unknown!',
-    };
-    
-    return callback(null, user, context);
   }
+  
+  context.idToken['https://cruise0.com/analytics'] = {
+    age_range: ageRange,
+  };
+  
+  return callback(null, user, context);
+}
